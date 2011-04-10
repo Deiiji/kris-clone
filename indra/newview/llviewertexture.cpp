af726e0329c5de44a8c15d99288a89c7c5898954
kirstenlee <kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-04-10 08:24:56 +0000

    Stop merging code from Viewer-Dev it May fix problems 'they' have but it breaks our code!

--- indra/newview/llviewertexture.cpp
+++ indra/newview/llviewertexture.cpp
@@ -1476,7 +1476,7 @@ BOOL LLViewerFetchedTexture::createTexture(S32 usename/*= 0*/)
 		setActive() ;
 	}
 
-	if (!needsToSaveRawImage())
+	if (!mForceToSaveRawImage)
 	{
 		mNeedsAux = FALSE;
 		destroyRawImage();
