b2d8698016646978c30b57b40ee308169c230bad
altair42 <altair42@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-05-07 00:47:38 +0000

    S21J for linux

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -955,9 +955,10 @@ class Linux_i686Manifest(LinuxManifest):
             self.path("libdirectfb-1.4.so.5.0.4")
             self.path("libfusion-1.4.so.5.0.4")
             self.path("libdirect-1.4.so.5.0.4")
+            self.path("libopenjpeg.so.2.0.6")
             self.path("libopenjpeg.so.2.0")
             self.path("libopenjpeg.so.2")
-            self.path("libopenjpeg.so.2.0.5", "libopenjpeg.so")
+	    self.path("libopenjpeg.so")
             self.path("libalut.so")
             self.path("libopenal.so", "libopenal.so.1")
             self.path("libopenal.so", "libvivoxoal.so.1") # vivox's sdk expects this soname
