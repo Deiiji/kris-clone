3cff533976995d867971f6ea13a53c53cdb23555
kirstenlee <kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-05-16 15:42:17 +0000

    mesh update to how uploaded objects are handled tested and seems to solve some issues :)

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -307,7 +307,7 @@ BOOL gLogoutInProgress = FALSE;
 
 ////////////////////////////////////////////////////////////
 // Internal globals... that should be removed
-static std::string gArgs = "Build 7";
+static std::string gArgs = "Build 8 Test";
 
 const std::string MARKER_FILE_NAME("SecondLife.exec_marker");
 const std::string ERROR_MARKER_FILE_NAME("SecondLife.error_marker");
